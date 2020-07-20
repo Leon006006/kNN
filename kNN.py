6608df6d70766d6c290b05ab22aa1b880c8089cf
Leon006006 <50409264+Leon006006@users.noreply.github.com>
2020-07-20 12:16:04 +0200

    final commented version

--- kNN.py
+++ kNN.py
@@ -3,24 +3,45 @@ import numpy as np
 
 class kNN():
     def __init__(self, data, class_tags, numb_classes):
+        """
+        :param data: n Training Vectors in R^m
+        :param class_tags: n Class tags for the training vectors
+        :param numb_classes: total number of classes
+        """
         self.T = data
         self.T_class = class_tags
+        self.numb_T = self.T.shape[1]  # safe number of training-vectors
         self.numb_classes = numb_classes
 
     def classify(self, vector, kNN):
-        numb_T = self.T.shape[1]
-        distance = np.zeros((1, numb_T))
+        """
+        :param vector: R^m Vector to classify
+        :param kNN: number of next neighbours to look at
+        :return: returns the class the vector belongs to
+        """
+        # Vector to safe all distances
+        distance = np.zeros((1, self.numb_T))
 
-        for x in range(0, numb_T):
+        # compute distance between all vectors and the vector to classify
+        for x in range(0, self.numb_T):
+            # euclidean distance
             distance[0, x] = np.linalg.norm(np.subtract(vector, self.T[:, x].reshape(self.T.shape[0], 1)), 2)
 
+        # sorting distance from small to big
+        # and saving the indices
         sorted_dist_index = np.argsort(distance)
+        # take the first k indices from sorted index array
         kNN_Vec = sorted_dist_index[0, :kNN]
-        class_counter = np.zeros((1, self.numb_classes))
 
+        # Count the number of times
+        # a certain class is in the k next neighbours
+        # of our vector to classify
+        class_counter = np.zeros((1, self.numb_classes))
         for x in range(0, kNN):
             class_index = self.T_class[0, kNN_Vec[x]]
             class_counter[0, class_index] += 1
 
+        # final class is most often next
+        # to the vector to classify
         final_class = np.argmax(class_counter)
-        print("Objekt gehört zu Klasse {}".format(final_class))
+        return final_class
--- kNN_test.py
+++ kNN_test.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import kNN
 
-
+# Producing a grid of equidistant data in [0,1] x [0,1]
 Data = np.zeros((2, 100))
 Data_tag = np.zeros((1, 100))
 for x in range(0, 10):
@@ -12,12 +12,25 @@ for x in range(0, 10):
             Data_tag[:, x * 10 + y] = 0
         else:
             Data_tag[:, x * 10 + y] = 1
-"""
-print(Data_tag)
-plt.plot(Data[0, :], Data[1, :])
-plt.show()
-"""
 
+# Plotting trainings-data
+for points in range(0, Data.shape[1]):
+    if Data_tag[0, points] == 0:
+        plt.plot(Data[0, points], Data[1, points], 'bo', label='Class 0')
+    else:
+        plt.plot(Data[0, points], Data[1, points], 'ro', label='Class 1')
+
+# Initialize the classifier
 kNN1 = kNN.kNN(Data, Data_tag.astype(int), 2)
-test = np.array([[0.6], [0.5]])
-kNN1.classify(test, 5)
+
+# Initialize the vector to test
+test = np.array([[0.55], [0.55]])
+
+# Plot point to classify
+plt.plot(test[0, 0], test[1, 0], 'gp', markersize=10, label='Test-Vector')
+plt.show()
+
+# Compute class
+classified = kNN1.classify(test, 5)
+
+print("Object belongs to class {}".format(classified))
